d0e42d020d9135d3286b48b503d7a5ce68129c1f
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2022-04-06 11:23:27 -0300

    requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,15 @@
+# módulo para recebermos valores externos
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file.endswith(".txt"):
+        try:
+            with open(path_file, "r") as file:
+                lines = file.read()
+                return lines.split('\n')
+        except OSError:
+            print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+    else:
+        return print("Formato inválido", file=sys.stderr)
+# file=sys.stderr: modificar a saída padrão para a saída de erros
