3b066e2e5c4b034c2aac93a571ea62545d0e8ad6
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2022-04-07 11:21:49 -0300

    inicia requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    lines_in_file = txt_importer(path_file)
+    content = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines_in_file),
+            "linhas_do_arquivo": lines_in_file,
+    }
+    print(content, file=sys.stdout)
+    instance.enqueue(path_file)
+    return content
+# https://pythonguides.com/python-stderr-stdin-and-stdout/
+# Python stdout é conhecido como saída padrão.
+# Possui função write imprimi diretamente qualquer string.
 
 
 def remove(instance):
